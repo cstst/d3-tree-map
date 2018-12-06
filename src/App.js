9f708ec2381302e0b10d32dd1edaf8e4fa89c644
Derek L <derekslarson@gmail.com>
2018-12-06 13:36:14 +0700

    map displayed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import * as d3 from "d3";
+import "d3-selection-multi";
 import "./App.css";
 
 class App extends Component {
@@ -9,6 +11,7 @@ class App extends Component {
     const movies = await fetch(
       "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json"
     ).then(res => res.json());
+    /*
     const genres = movies.children;
     console.log(genres);
     const genreTotals = genres.map(genre => {
@@ -19,15 +22,56 @@ class App extends Component {
       );
       return { name, total };
     });
-    console.log(genreTotals);
+    */
+    this.drawMap(movies);
   }
 
+  drawMap = data => {
+    const height = 900;
+    const width = 1200;
+    const x = d3.scaleLinear().range([0, width]);
+    const y = d3.scaleLinear().range([0, height]);
+    const margin = {
+      top: 100,
+      right: 120,
+      bottom: 150,
+      left: 120
+    };
+    const root = d3.hierarchy(data);
+    root.sum(d => d.value);
+
+    const treemap = d3
+      .treemap()
+      .size([height, width])
+      .paddingOuter(10);
+
+    treemap(root);
+
+    const svg = d3
+      .select("#tree-map")
+      .append("svg")
+      .attrs({
+        height,
+        width
+      })
+      .append("g");
+
+    d3.select("svg g")
+      .selectAll("rect")
+      .data(root.descendants())
+      .enter()
+      .append("rect")
+      .attrs({
+        class: "rect",
+        x: d => d.x0,
+        y: d => d.y0,
+        width: d => d.x1 - d.x0,
+        height: d => d.y1 - d.y0
+      });
+  };
+
   render() {
-    return (
-      <div>
-        <div id="tree-map" />
-      </div>
-    );
+    return <div id="tree-map" />;
   }
 }
 
