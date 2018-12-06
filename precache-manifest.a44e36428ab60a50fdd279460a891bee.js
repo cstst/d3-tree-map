9a71215122c4f17b34e1d8d6434defa43d6813a0
Derek L <derekslarson@gmail.com>
2018-12-06 20:49:38 +0700

    Updates

---
+++ precache-manifest.a44e36428ab60a50fdd279460a891bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "696129f51ba7ae6d0e72",
+    "url": "/static/js/main.696129f5.chunk.js"
+  },
+  {
+    "revision": "8d636aedcc83198968d3",
+    "url": "/static/js/1.8d636aed.chunk.js"
+  },
+  {
+    "revision": "696129f51ba7ae6d0e72",
+    "url": "/static/css/main.6efdca73.chunk.css"
+  },
+  {
+    "revision": "3fd66d4e33e53d0416c12e48ce5bce72",
+    "url": "/index.html"
+  }
+];
