0dac891800610a8450b0585ef26c985288ca917f
Derek L <derekslarson@gmail.com>
2018-12-06 13:04:22 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    data: null
+  };
+  async componentDidMount() {
+    const movies = await fetch(
+      "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json"
+    ).then(res => res.json());
+    const genres = movies.children;
+    console.log(genres);
+    const genreTotals = genres.map(genre => {
+      const name = genre.name;
+      const total = genre.children.reduce(
+        (acc, val) => acc + parseInt(val.value),
+        0
+      );
+      return { name, total };
+    });
+    console.log(genreTotals);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div id="tree-map" />
       </div>
     );
   }
